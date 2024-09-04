d54224c2857ffd26b2028f7264362d81e46e0449
sDcy2002 <nattapong855121913@gmail.com>
2024-09-04 11:01:52 +0700

    backend

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-var bodyParser = require('body-parsar');
+var bodyParsar = require('body-parsar');
 
 const base_url = "http://localhost:3000"
 
