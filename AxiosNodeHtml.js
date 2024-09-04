975e0e0b29e8e7d7beceb9ba704e15808f9d5fff
sDcy2002 <nattapong855121913@gmail.com>
2024-09-04 11:08:18 +0700

    frontend

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-var bodyParsar = require('body-parsar');
+var bodyParser = require('body-parser');
 
 const base_url = "http://localhost:3000"
 
 app.set('view engine', 'ejs');
-app.use(bodyParsar.json());
-app.use(bodyParsar.urlencoded({ extended: false} ));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false} ));
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
